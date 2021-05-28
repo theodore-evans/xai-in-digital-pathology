6e72bef091c96e89108c350ca53c128d1e62dbb7
Theodore Evans <theodore.evans@dai-labor.de>
2021-05-28 16:09:19 +0200

    added dynamic instructions and example shuffling

---
+++ src/CreateSurvey.js
@@ -0,0 +1,144 @@
+import { questions, instructions } from "./SurveyContent.js"
+
+const REQUIRED_TEXT = "*"
+const QUESTIONS_ARE_ON_NEW_LINE = false
+const ANSWERS_ARE_REQUIRED = false
+const COMMENT_ROWS = 3
+const SHUFFLE_EXAMPLES = true
+
+function createCommentsBox(questionId) {
+    return (
+        {
+            "type": "comment",
+            "name": questionId + "_comments",
+            "title": "Comments",
+            "rows": COMMENT_ROWS
+        }
+    )
+}
+
+function createUserProfilingPage() {
+
+    const USER_PROFILING_PAGE_ID = "user_profiling"
+
+    var userProfilingPage = {
+        "name": USER_PROFILING_PAGE_ID,
+        "elements": []
+    }
+
+    for (var userProfilingQuestion of questions.userProfilingQuestions) {
+        userProfilingPage.elements.push({
+            "type": "rating",
+            "name": userProfilingQuestion.id,
+            "title": userProfilingQuestion.text,
+            "rateMax": questions.rateMax,
+            "minRateDescription": questions.minRateDescription,
+            "maxRateDescription": questions.maxRateDescription,
+            "isRequired": ANSWERS_ARE_REQUIRED
+        })
+    }
+
+    userProfilingPage.elements.push(createCommentsBox(USER_PROFILING_PAGE_ID))
+
+    return userProfilingPage
+}
+
+function createExamplePage(example) {
+
+    var examplePage = {
+        "name": "example_" + example.id,
+        "title": example.description,
+        "elements": [
+            {
+                "type": "panel",
+                "elements": [
+                    {
+                        "type": "image",
+                        "imageLink": example.outputImage,
+                        "imageWidth": "256px",
+                        "imageHeight": "256px",
+                    },
+                    {
+                        "type": "html",
+                        "html": example.outputDescription
+                    }
+                ],
+                "width": "300px"
+            },
+            {
+                "type": "panel",
+                "elements": [
+                    {
+                        "type": "image",
+                        "imageLink": example.explanationImage,
+                        "imageWidth": "256px",
+                        "imageHeight": "256px",
+                    },
+                    {
+                        "type": "html",
+                        "html": example.explanationDescription
+                    }
+                ],
+                "width": "300px",
+                "startWithNewLine": false
+            }
+        ]
+    }
+
+    var ratingPanel = {
+        "type": "panel",
+        "elements": [],
+        "startWithNewLine": QUESTIONS_ARE_ON_NEW_LINE
+    }
+
+    for (var ratingQuestion of questions.ratingQuestions) {
+        ratingPanel.elements.push(
+            {
+                "type": "rating",
+                "name": example.id + "_" + ratingQuestion.id,
+                "title": ratingQuestion.text,
+                "rateMax": questions.rateMax,
+                "minRateDescription": questions.minRateDescription,
+                "maxRateDescription": questions.maxRateDescription,
+                "isRequired": ANSWERS_ARE_REQUIRED
+            }
+        )
+    }
+
+    ratingPanel.elements.push(createCommentsBox(example.id))
+    examplePage.elements.push(ratingPanel)
+
+    return examplePage
+}
+
+var surveyJson = {
+    "requiredText": REQUIRED_TEXT,
+    "pages": [
+        {
+        }
+    ]
+}
+
+surveyJson.pages.push(createUserProfilingPage())
+
+surveyJson.pages.push(
+    {
+        "name": "info_page",
+        "elements": [
+            {
+                "type": "html",
+                "html": instructions
+            }
+        ]
+    }
+);
+
+function randomize(a, b) {
+    return Math.random() - 0.5;
+}
+
+for (var example of SHUFFLE_EXAMPLES ? questions.examples.sort(randomize) : questions.examples) {
+    surveyJson.pages.push(createExamplePage(example))
+}
+
+export { surveyJson };
