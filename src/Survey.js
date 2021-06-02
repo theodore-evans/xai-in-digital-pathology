2759ef2a4b311abd54b61911a071bb1372debb32
CarlOrge.Retzlaff <carlorge.retzlaff@dai-labor.de>
2021-06-02 16:54:39 +0200

    Exporting survey results to API

--- src/Survey.js
+++ src/Survey.js
@@ -8,38 +8,57 @@ import Images from "./assets";
 
 Survey.StylesManager.applyTheme("default");
 
-function onValueChanged(result) {
-	console.log("value changed!");
+function onValueChanged(sender, options) {
+  console.log(sender, options);
+  let question_class = class_regex.exec(options.question.name)[0];
+
+  if (Survey.data[question_class]) {
+    Survey.data[question_class][options.name] = options.value;
+  } else {
+    Survey.data[question_class] = {};
+    Survey.data[question_class][options.name] = options.value;
+  }
 }
 
-function onComplete(result) {
-	console.log("Complete! " + result);
+function onComplete() {
+  fetch("http://localhost:5000/result", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Survey.data),
+  });
 }
+
+//setup the survey data
+Survey.data = {};
+let class_regex = /^(.*?)\_/;
 
 export function SurveyPage() {
-	var model = new Survey.Model(surveyJson);
-	model.showProgressBar = "bottom";
-	model.showQuestionNumbers = "off";
-	model.onAfterRenderQuestion.add(function (sender, options) {
-		if (options.question.name === "displayExample") {
-			var img = options.htmlElement.getElementsByTagName("img")[0];
-			img.src = Images.example_app_output;
-			options.htmlElement.onmouseover = function () {
-				img.src = options.question.imageLink;
-			};
-			options.htmlElement.onmouseout = function () {
-				img.src = Images.example_app_output;
-			};
-		}
-	});
-	return (
-		<div className="container">
-			<h2>Explainable AI for digital pathology</h2>
-			<Survey.Survey
-				model={model}
-				onComplete={onComplete}
-				onValueChanged={onValueChanged}
-			/>
-		</div>
-	);
+  var model = new Survey.Model(surveyJson);
+  Survey.data["id"] = Date.now();
+  model.showProgressBar = "bottom";
+  model.showQuestionNumbers = "off";
+  model.onAfterRenderQuestion.add(function (sender, options) {
+    if (options.question.name === "displayExample") {
+      var img = options.htmlElement.getElementsByTagName("img")[0];
+      img.src = Images.example_app_output;
+      options.htmlElement.onmouseover = function () {
+        img.src = options.question.imageLink;
+      };
+      options.htmlElement.onmouseout = function () {
+        img.src = Images.example_app_output;
+      };
+    }
+  });
+  return (
+    <div className="container">
+      <h2>Explainable AI for digital pathology</h2>
+      <Survey.Survey
+        model={model}
+        onComplete={onComplete}
+        onValueChanged={onValueChanged}
+      />
+    </div>
+  );
 }
