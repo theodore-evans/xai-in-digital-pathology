cb4a46586e7438cee85b0743fa4e180c20579ac1
Theodore Evans <theodore.evans@dai-labor.de>
2021-06-07 15:02:26 +0200

    modified backup script in line with mountpoints:

--- backup-mongodb.sh
+++ backup-mongodb.sh
@@ -4,4 +4,4 @@ export CONTAINER_NAME="mongo"
 export DATABASE_NAME="xai_surveys"
 export TIMESTAMP=$(date +'%Y%m%d%H%M%S')
 
-docker exec -t ${CONTAINER_NAME} mongodump --out /data/${DATABASE_NAME}-backup-${TIMESTAMP} --db ${DATABASE_NAME}
+docker exec -t ${CONTAINER_NAME} mongodump --out /data/db/${DATABASE_NAME}-backup-${TIMESTAMP} --db ${DATABASE_NAME}
