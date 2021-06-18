b92c55b24aa995f9720a42285d1d9d3340f642b2
CarlOrge.Retzlaff <carlorge.retzlaff@dai-labor.de>
2021-06-18 16:41:57 +0200

    added graphics and updated script

--- backup-mongodb.sh
+++ backup-mongodb.sh
@@ -5,3 +5,6 @@ export DATABASE_NAME="xai_surveys"
 export TIMESTAMP=$(date +'%Y%m%d%H%M%S')
 
 docker exec -t ${CONTAINER_NAME} mongodump --out /data/db/${DATABASE_NAME}-backup-${TIMESTAMP} --db ${DATABASE_NAME}
+#command to dump only relevant docs to stdout
+#docker exec -t mongo  mongoexport --db xai_surveys --collection surveys --query '{"id":{"$gte":1623319499818}}'
+
